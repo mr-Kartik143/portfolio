ba089fc39243ae7fdcca55aa79e8a372dc6db0d3
kartik <mrkartikauti@gmail.com>
2023-01-03 14:36:49 +0530

    test

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -4,6 +4,7 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div className="container contactbox" id="contact">
+    <h1>checking </h1>
       <p className="text-danger ffont fs-2 text-center mt-5">Contact me</p>
       <h5 className="text-center container text-light ffont mb-4">
         Get In Touch
@@ -57,7 +58,6 @@ export default function Contact() {
             href="mailto:mrkartikauti@gmail.com"
             className="rfont text2 mx-5 txtdoc"
           >
-            Click Here
           </a>
         </div>
         <div>
@@ -68,7 +68,7 @@ export default function Contact() {
             fill="currentColor"
             className="bi bi-telephone-fill icon"
             viewBox="0 0 16 16"
-          >
+          >click Here
             <path
               fillRule="evenodd"
               d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
