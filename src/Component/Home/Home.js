6bad31e43533c65e02f8a28e67bce3e37c93c383
kartik <mrkartikauti@gmail.com>
2023-01-03 14:22:00 +0530

    testing commit

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="container mainbox" id="home">
-      <h2 className="firsttext text-center mt-5">Hello My Name is</h2>
+      <h2 className="firsttext text-center mt-5">Hello My Name</h2>
       <h1 className="nametext text-center m-3 text-danger">Kartik Auti.</h1>
    <p className="hometxt mx-5 mb-5 mt-2">     i'am Strong in design and integration with intuitive problem-solving
         skills. Proficient in JAVA, C, JAVASCRIPT, HTML & CSS, REACT JS, and
